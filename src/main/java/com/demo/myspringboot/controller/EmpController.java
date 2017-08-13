e80a4c2666eff2fc879c51505c4ce69b813596f3
angularadmin <Zen_json@163.com>
2017-08-13 12:08:34 +0800

    //修改

---
+++ src/main/java/com/demo/myspringboot/controller/EmpController.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright: Copyright (C) 2006
+ * Company: XXX技术有限公司
+ * author:Administrator
+ * DateTime:2017/8/13 0013
+ */
+package com.demo.myspringboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "")
+public class EmpController {
+
+}
